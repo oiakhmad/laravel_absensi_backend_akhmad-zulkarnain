96e0fc13abcd3d9a6cbe02abd8dea30c8a478ca9
oiakhmad <akhmad.py@gmail.com>
2024-05-13 22:09:29 +0700

    add feature list (read,Create) note

--- routes/api.php
+++ routes/api.php
@@ -14,4 +14,6 @@ Route::get('/company', [App\Http\Controllers\Api\CompanyController::class, 'show
 Route::post('/checkin', [App\Http\Controllers\Api\AttendanceController::class, 'checkin'])->middleware('auth:sanctum');
 Route::post('/checkout', [App\Http\Controllers\Api\AttendanceController::class, 'checkout'])->middleware('auth:sanctum');
 Route::post('/update-profile', [App\Http\Controllers\Api\AuthController::class, 'updateProfile'])->middleware('auth:sanctum');
+// apiResource for initial all method (GET,PUT,POST,DELETE)
 Route::apiResource('/api-permissions', App\Http\Controllers\Api\PermissionController::class)->middleware('auth:sanctum');
+Route::apiResource('/api-notes', App\Http\Controllers\Api\NoteController::class)->middleware('auth:sanctum');
