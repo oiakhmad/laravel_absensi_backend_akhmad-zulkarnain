b58fb5bebae35a6bf46c06c7bca278c1ea919dc2
oiakhmad <akhmad.py@gmail.com>
2024-06-06 20:30:32 +0700

    add route checkin

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,5 @@ Route::post('/update-profile', [App\Http\Controllers\Api\AuthController::class, 
 // apiResource for initial all method (GET,PUT,POST,DELETE)
 Route::apiResource('/api-permissions', App\Http\Controllers\Api\PermissionController::class)->middleware('auth:sanctum');
 Route::apiResource('/api-notes', App\Http\Controllers\Api\NoteController::class)->middleware('auth:sanctum');
+//is checkin
+Route::get('/is-checkin', [App\Http\Controllers\Api\AttendanceController::class, 'isCheckedin'])->middleware('auth:sanctum');
